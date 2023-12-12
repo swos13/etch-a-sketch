0b31774bb7a6aad7d14fcb64e6a3a203e9a8b0db
swos <szymon.wos1319@gmail.com>
2023-12-12 14:14:59 +0100

    Add input determining length of grid

--- script.js
+++ script.js
@@ -1,19 +1,36 @@
 let grid = document.querySelector('.grid');
+const button = document.querySelector('#create');
 
 
-for(let i=0; i<16; i++){
-    let row = document.createElement('div');
-    for(let j=0; j<16; j++){
-        let box = document.createElement('div');
-        box.style.border = '1px solid black';
-        box.style.minWidth = '32px';
-        box.style.minHeight = '32px';
-        box.addEventListener('mouseenter', () => {
-            box.style.backgroundColor = 'black';
-        })
-        row.appendChild(box);
-    }
-    row.style.display = 'flex';
-    row.className = 'row';
-    grid.appendChild(row);
+function clearGrid(){
+    while(grid.firstChild)
+        grid.removeChild(grid.firstChild);
 }
+
+
+button.addEventListener('click', () =>{
+
+    clearGrid();
+
+    let number = parseInt(document.querySelector('input').value);
+
+    for(let i=0; i<number; i++){
+        let row = document.createElement('div');
+        for(let j=0; j<number; j++){
+            let box = document.createElement('div');
+            box.style.border = '1px solid black';
+            box.style.minWidth = '32px';
+            box.style.minHeight = '32px';
+            box.addEventListener('mouseenter', () => {
+                box.style.backgroundColor = 'black';
+            })
+            row.appendChild(box);
+        }
+        row.style.display = 'flex';
+        row.className = 'row';
+        grid.appendChild(row);
+    }
+    
+});
+
+
